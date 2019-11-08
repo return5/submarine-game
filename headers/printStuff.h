b716d3834738fed4a36b21b8c6a8e41a066f32f7
chris <44552743+return5@users.noreply.github.com>
2019-11-08 16:31:45 -0600

    Update printStuff.h

--- headers/printStuff.h
+++ headers/printStuff.h
@@ -1,16 +1,12 @@
-void updateAPDisplay(void);
-void updateLastDetected(void);
-void updateAOETorDisplay(void);
-void updateHealthDisplay(void);
-void updateLocationDisplay(void);
+#include "units.h"
+void updateBoard(void);
 void printToMain(const int x, const int y, const char *const str);
 void printToTxtScr(const int x, const int y, const char *const str);
-void printToOptWin(const int x, const int y, const char *const str);
 void printPieces(void);
-void printPlayerSub(void);
-void printOptWin(void);
-void displayAOE(const int limit);
+void displayAOE(const int limit,const int color, const int delay);
 void displaySonar(void);
 void printNumSign(const int i,const int j, const int x, const int y, const int color);
-void printLastDetected(SHIP *const ship);
 void animateTorpedo(const int limit);
+void printToOptWin(const int x, const int y, const char *const str);
+void printOptWin(void);
+void printLastDetected(SHIP *const ship);
