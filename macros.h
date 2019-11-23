9c4550b5bc7f8c508257c029b6658f6db2e4e950
chris <44552743+return5@users.noreply.github.com>
2019-11-23 09:58:55 -0600

    Delete macros.h

--- macros.h
+++
@@ -1,9 +0,0 @@
-#define TORPEDODISTANCE 10 //distance a torpedo can fire.
-#define SHIP_1 enemies->ship
-#define SHIP_2 enemies->next->ship
-#define SHIP_3 enemies->next->next->ship
-#define PLAYER player_sub->ship->sub
-#define Y_EDGE 25
-#define X_EDGE 75
-#define X_NORM 2  //value to make spacing of x coordinate look like spacing of y coordinate
-enum COLORS {RED_COLOR = 1,GREEN_COLOR = 2,BLUE_COLOR = 3,YELLOW_COLOR = 4};
