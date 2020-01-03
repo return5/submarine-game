cc66c8a198e2bbf0a16329c864fc329365a5eb4f
chris <44552743+return5@users.noreply.github.com>
2020-01-02 20:13:00 -0600

    Update main-0.8.c

--- src/main-0.8.c
+++ src/main-0.8.c
@@ -4,7 +4,21 @@
 //version: 0.8
 //currently a work in progress.
 //turned based game. player controls a submarine and is in a fight against an enemy submarine and an enemy destroyer.  also includes a cargo ship which can be destroyed for extra points
+ /* 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 //---------------------------------------- headers ------------------------------------------------
 #include <stdlib.h>
